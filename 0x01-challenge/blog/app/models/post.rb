59f7029e2a32c6977588a52a1c5d334289f178ea
NickForero11 <nicolasforero1199@gmail.com>
2020-02-23 21:10:24 -0500

    Added react-blog and deleted wrong blog

--- 0x01-challenge/blog/app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ApplicationRecord
-  @online = true
-  has_many  :comments, dependent: :destroy
-  validates :title, presence: true, length: {minimum: 5}
-	validates :body,  presence: true
-end
