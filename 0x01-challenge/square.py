cebaad5fd1947a6d496bd3e8819d2743d88884e3
NickForero11 <nicolasforero1199@gmail.com>
2020-02-23 21:58:51 -0500

    final version

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,62 +1,59 @@
 #!/usr/bin/python3
-"""Module with a basic Square Class
+"""
+documentation
 """
 
 
 class Square:
-    """Square class with Width and Height.
+    ''' Square class '''
 
-    Keyword arguments:
-    width(int): X size.
-    height(int): Y size.
-    """
     width = 0
     height = 0
 
-    def __param_validator(self, name, value):
-        """Checks if its a valid parameter value"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if not value > 0:
-            raise ValueError("{} must be > 0".format(name))
-
     def __init__(self, width, height):
-        """Initialize an empty or specific Square"""
+        ''' initializer for attributes '''
         self.width = width
         self.height = height
 
+    def __wh_validator(self, name, value):
+        ''' comments '''
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+
     @property
     def width(self):
-        """Width getter"""
+        ''' comments '''
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Width setter"""
-        self.__param_validator("width", value)
+        ''' comments '''
+        self.__wh_validator("width", value)
         self.__width = value
 
     @property
     def height(self):
-        """Height getter"""
+        ''' comments '''
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Height setter"""
-        self.__param_validator("height", value)
+        ''' comments '''
+        self.__wh_validator("height", value)
         self.__height = value
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """ Area of the square calculates area """
         return self.width * self.height
 
-    def perimeter_of_my_square(self):
-        """Compute the perimeter of a Square"""
+    def permiter_of_my_square(self):
+        ''' Perimeter of Square '''
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """Returns an human readable representation of a Square"""
+        ''' str representation of instance '''
         return "{}/{}".format(self.width, self.height)
 
 
